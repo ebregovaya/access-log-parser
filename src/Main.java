6d384fa49e936ad4e4e45046063940d36a94c7ce
eberegovaya <beregovayaelena123@gmail.com>
2026-01-12 21:54:24 +0300

    Изменена логика проекта

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println("Введите текст и нажмите <Enter>:");
+        String text=new Scanner(System.in).nextLine();
+        System.out.println("Длина текста"+text.length());
 
         }
     }
