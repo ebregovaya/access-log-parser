6bdbb13ceaaaf2bf095754b94a8d5a5e8a7173c9
eberegovaya <beregovayaelena123@gmail.com>
2026-02-02 11:47:22 +0300

    Main.java оптимизирован

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class Main {public static void main(String[] args) {
-    //C:\_git\AccessLogParser\src\Main.java
-    int res=0;
-         while (true) {
+public class Main {
+    public static void main(String[] args) {
+        //C:\_git\AccessLogParser\src\Main.java
+        int res = 0;
+        while (true) {
             System.out.println("Для выхода нажмите 0");
             System.out.println("Укажите путь к файлу :");
             String path = new Scanner(System.in).nextLine();
@@ -14,22 +15,20 @@ public class Main {public static void main(String[] args) {
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
-             if (file.isDirectory()) {
-                 System.out.println("Это директория ");
-             }
-              else if (file.exists()) {
+            if (isDirectory) {
+                System.out.println("Это директория ");
+                continue;
+            }
+            if (fileExists) {
                 System.out.println("Путь указан верно: " + file.getAbsolutePath());
-                 res++;
-                 System.out.println("Это файл номер " + res);
-              }
-               else {
-                 System.out.println("Файл не существует  " + file.getAbsolutePath());
-
-               }
-          }
-
-         }
+                res++;
+                System.out.println("Это файл номер " + res);
+                continue;
+            }
+            System.out.println("Файл не существует  " + file.getAbsolutePath());
         }
+    }
+}
 
 
 
